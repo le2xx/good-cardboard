d39bfc3f73b1fb37b9dd807f98be052729135810
le2xx <le2x@rambler.ru>
2018-11-23 15:20:00 +0300

    Created modal window

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
 import '../styles/index.scss';
+
+const modalWindow = document.querySelector('.modal');
+const modalWindowClose = document.querySelector('.modal__close');
+const btnCall = document.querySelector('.logo-block__btn-call');
+
+modalWindowClose.addEventListener('click', () => modalWindow.style.display = 'none');
+btnCall.addEventListener('click', () => modalWindow.style.display = 'block');
