6d9b90f40bca8c4dfaacb70c8b11e1d2401b6c65
le2xx <le2x@rambler.ru>
2018-12-04 15:36:37 +0300

    Created slider block

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
+import {tns} from 'tiny-slider';
 
 const modalWindow = document.querySelector('.modal');
 const modalWindowClose = document.querySelector('.modal__close');
@@ -6,3 +7,27 @@ const btnCall = document.querySelector('.logo-block__btn-call');
 
 modalWindowClose.addEventListener('click', () => modalWindow.style.display = 'none');
 btnCall.addEventListener('click', () => modalWindow.style.display = 'block');
+
+const slider = tns({
+  "container": "#customize",
+  "items": 1,
+  "controlsContainer": "#customize-controls",
+  "navContainer": "#customize-thumbnails",
+  "navAsThumbnails": true,
+  "autoplay": true,
+  "autoplayButton": "#customize-toggle",
+  "swipeAngle": false,
+  "speed": 1000,
+  "gutter": 5,
+});
+
+window.onload = () => {
+  slider;
+};
+
+const btnPrev = document.querySelector('.slider__btn-prev');
+const btnNext = document.querySelector('.slider__btn-next');
+const btnThumb = document.querySelector('.slider__thumbnails');
+btnPrev.addEventListener('click', () => setTimeout(() => slider.play(), 1000));
+btnNext.addEventListener('click', () => setTimeout(() => slider.play(), 1000));
+btnThumb.addEventListener('click', () => setTimeout(() => slider.play(), 1000));
